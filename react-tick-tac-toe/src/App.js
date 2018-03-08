f602fd52ec5a117e45106d27de32693a45d5d681
Luke <luke.michals@gmail.com>
2018-03-07 19:13:08 -0800

    complete game logic

--- react-tick-tac-toe/src/App.js
+++ react-tick-tac-toe/src/App.js
@@ -2,41 +2,92 @@ import React, { Component } from "react";
 import "./App.css";
 import Row from "./Row";
 
+const initialState = {
+  boardState: [
+    { id: 0, st: null },
+    { id: 1, st: null },
+    { id: 2, st: null },
+    { id: 3, st: null },
+    { id: 4, st: null },
+    { id: 5, st: null },
+    { id: 6, st: null },
+    { id: 7, st: null },
+    { id: 8, st: null }
+  ],
+  turn: "X",
+  message: "",
+  gameOver: false
+};
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      boardState: [
-        { id: 0, st: null },
-        { id: 1, st: null },
-        { id: 2, st: null },
-        { id: 3, st: null },
-        { id: 4, st: null },
-        { id: 5, st: null },
-        { id: 6, st: null },
-        { id: 7, st: null },
-        { id: 8, st: null }
-      ],
-      turn: "X",
-      message: ""
+      ...initialState,
+      boardState: [...initialState.boardState]
     };
   }
 
-  changeTurn = () => {
-    const upNext = this.state.turn === "X" ? "O" : "X";
-    this.setState({ turn: upNext });
-  };
+  /**
+   * 0 1 2
+   * 3 4 5
+   * 6 7 8
+   */
+
+  winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
   handleSquareClick = id => {
-    console.log(`${id} clicked`);
-    const { boardState } = this.state;
-    
+    const { boardState, turn, gameOver } = this.state;
+
+    if (gameOver) return;
+
+    const upNext = turn === "X" ? "O" : "X";
+
     // check if square has already been played
-    if(!boardState[id].st) {
-      this.setState({message: "That square has already been played"})
+    if (boardState[id].st) {
+      this.setState({ message: "That square has already been played" });
       return;
     }
-    
+    const newBoardState = [
+      ...boardState.slice(0, id),
+      { id, st: turn },
+      ...boardState.slice(id + 1)
+    ];
+
+    this.setState(
+      { boardState: newBoardState, turn: upNext, message: "" },
+      () => this.checkForWinner(turn)
+    );
+  };
+
+  checkForWinner = possibleWinner => {
+    const { boardState } = this.state;
+    const winnerFound = this.winningCombos.some(combo => {
+      return combo.every(squareId => {
+        return boardState[squareId].st === possibleWinner;
+      });
+    });
+    if (winnerFound)
+      this.setState({
+        message: `${possibleWinner} is the winner!`,
+        gameOver: true
+      });
+  };
+
+  resetBoard = () => {
+    this.setState({
+      ...initialState,
+      boardState: [...initialState.boardState]
+    });
   };
 
   render() {
@@ -50,17 +101,22 @@ class App extends Component {
       <div className="App">
         <table>
           <tbody>
-            {dataByRows.map((rowData, i) => (
+            {dataByRows.map((rowData, i) =>
               <Row
                 key={i}
                 rowData={rowData}
                 handleSquareClick={this.handleSquareClick}
               />
-            ))}
+            )}
           </tbody>
         </table>
-        <p>It's currently {this.state.turn}'s turn</p>
-        <p>{this.state.message}</p>
+        <p>
+          It's currently {this.state.turn}'s turn
+        </p>
+        <button onClick={this.resetBoard}>Reset Board</button>
+        <p>
+          {this.state.message}
+        </p>
       </div>
     );
   }
